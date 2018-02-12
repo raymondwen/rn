17725706a292f9e6f5588a6f90064b73fc3ad0d4
Raymond Wen <yfwen@outlook.com>
2018-02-12 14:25:20 -0600

    Learn state and Redux

--- my-app/App.js
+++ my-app/App.js
@@ -1,3 +1,44 @@
+
+// State https://facebook.github.io/react-native/docs/state.html
+// Redux could be also useful: https://redux.js.org/index.html
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
+
+/*
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 
@@ -20,7 +61,7 @@ export default class LotsOfGreetings extends Component {
     );
   }
 }
-
+*/
 /*
 import React, { Component } from 'react';
 import { AppRegistry, Image } from 'react-native';
