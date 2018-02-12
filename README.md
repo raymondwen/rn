f01fc967644a570b13e948b8d3e15c821c9350a6
Raymond Wen <raymond@coach360.net>
2018-02-12 13:55:48 -0600

    Learn the basics and Props

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rn
+This is a learning project based on this tutorial:
+https://facebook.github.io/react-native/docs/tutorial.html
+
+The goal is to learn all the basics of React Native, and submit my first app to
+Google play store.
--- my-app/App.js
+++ my-app/App.js
@@ -1,3 +1,30 @@
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
+
+export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+/*import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world????!!!---</Text>
+    );
+  }
+}
+*/
+
+/*
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -21,3 +48,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
