38cbc0352c3893310b6ff0b2e2d1250cf49214e4
Raymond Wen <yfwen@outlook.com>
2018-02-13 11:25:16 -0600

    Learn flexDirection

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
+export default class FlexDirectionBasics extends Component {
+  render() {
+    return (
+      // Try setting `flexDirection` to `column`.
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+
+/*import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
 export default class FlexDimensionsBasics extends Component {
   render() {
     return (
@@ -15,7 +31,7 @@ export default class FlexDimensionsBasics extends Component {
     );
   }
 }
-
+/*
 /*
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
