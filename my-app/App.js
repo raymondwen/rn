fdf650d08a1cdc8f56943ed5cf2d88ed02c54bb7
Raymond Wen <yfwen@outlook.com>
2018-02-13 12:02:05 -0600

    Learn flexDirection-2+3

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,52 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
+export default class AlignItemsBasics extends Component {
+  render() {
+    return (
+      // Try setting `alignItems` to 'flex-start'
+      // Try setting `justifyContent` to `flex-end`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+
+/*
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
+export default class JustifyContentBasics extends Component {
+  render() {
+    return (
+      // Try setting `justifyContent` to `center`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+    );
+  }
+};
+*/
+
+/*import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
 export default class FlexDirectionBasics extends Component {
   render() {
     return (
@@ -13,6 +59,7 @@ export default class FlexDirectionBasics extends Component {
     );
   }
 };
+*/
 
 /*import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
