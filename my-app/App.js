36c4aab0fb3df94b57bfdb1cf71869dcc1c24798
Raymond Wen <yfwen@outlook.com>
2018-02-12 18:50:03 -0600

    Learn style

--- my-app/App.js
+++ my-app/App.js
@@ -1,4 +1,33 @@
 
+//Style: https://facebook.github.io/react-native/docs/style.html
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+export default class LotsOfStyles extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+/*
 // State https://facebook.github.io/react-native/docs/state.html
 // Redux could be also useful: https://redux.js.org/index.html
 import React, { Component } from 'react';
@@ -37,6 +66,7 @@ export default class BlinkApp extends Component {
     );
   }
 }
+*/
 
 /*
 import React, { Component } from 'react';
