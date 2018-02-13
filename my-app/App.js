7b9c6e8b7dd07f9c240e296c3830ff3401c51232
Raymond Wen <yfwen@outlook.com>
2018-02-13 12:22:02 -0600

    Learn Touch/Button

--- my-app/App.js
+++ my-app/App.js
@@ -1,4 +1,60 @@
+//Touch: Button
+import React, { Component } from 'react';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
+
+/*
 // Textinput: https://facebook.github.io/react-native/docs/handling-text-input.html
 // More to read: https://reactjs.org/docs/forms.html#controlled-components
 // https://facebook.github.io/react-native/docs/handling-text-input.html
@@ -26,7 +82,7 @@ export default class PizzaTranslator extends Component {
     );
   }
 }
-
+*/
 /*
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
